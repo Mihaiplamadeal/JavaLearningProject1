cc5e7b3d3ced8e58ce9da7b5b65d141e70bff8d1
user <mplamadealaqa@gmail.com>
2023-09-20 13:57:14 +0300

    Implemented the AutoServiceTask.

---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Worker.java
@@ -0,0 +1,16 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Worker {
+
+    public String name;
+
+    public boolean isQualified;
+
+    public int age;
+
+    public Worker(){
+
+        System.out.println("Un obiect de tip worker este creat. Acest constructor este fara parametri. Nu uita sa populezi numele si varsta");
+    }
+
+}
